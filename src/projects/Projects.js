fa902a0006f0d03f5d41d9df47b4746ecaeac3ad
Sergey stava <santerakis@gmail.com>
2023-04-07 20:17:20 +0300

    scss

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import styles from './Projects.module.css'
+import styles from './Projects.module.scss'
 import Project from "./project/Project";
 import Title from "../common/components/title/Title";
+import socialImg from './../assets/images/social.jpg'
+import todoImg from './../assets/images/to-do-list-apps.jpg'
+
+const social = {
+    backgroundImage: `url(${socialImg})`,
+};
+const todo = {
+    backgroundImage: `url(${todoImg})`,
+};
+// const hw = {
+//     backgroundImage: 'url(' + imgUrl + ')',
+// };
+// const myProj = {
+//     backgroundImage: 'url(' + imgUrl + ')',
+// };
 
 const Projects = () => {
     return (
@@ -9,10 +24,10 @@ const Projects = () => {
             <div className={styles.projectsContainer}>
                 <Title text={'Projects'}/>
                 <div className={styles.projects}>
-                    <Project title={'Casino'} description={'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'}/>
-                    <Project title={'Kino'} description={'It is a long established fact that a reader will be distracted .'}/>
-                    <Project title={'Kino'} description={'It is a long established fact that a reader will be distracted .'}/>
-                    <Project title={'Kino'} description={'It is a long established fact that a reader will be distracted .'}/>
+                    <Project style={social} title={'Social'} description={'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'}/>
+                    <Project style={todo} title={'Todo'} description={'It is a long established fact that a reader will be distracted .'}/>
+                    <Project title={'Hw'} description={'It is a long established.'}/>
+                    <Project title={'My Project'} description={'It is a long established fact that a reader will be distracted .'}/>
                 </div>
             </div>
 
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import styles from './../project/Project.module.css'
+import styles from './Project.module.scss'
+
 
 const Project = (props) => {
     return (
         <div className={styles.project}>
-            <div className={styles.projImg}>
+            <div className={styles.projImg} style={props.style}>
                 <a className={styles.button} href={'##'}>Look</a>
             </div>
-            <h3>{props.title}</h3>
-            <span className={styles.description}>{props.description}</span>
+            <div className={styles.projInfo}>
+                <h3 className={styles.projTitle}>{props.title}</h3>
+                <span className={styles.description}>{props.description}</span>
+            </div>
         </div>
     );
 };
